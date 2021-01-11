a68b93084ae4214d0a9e2f46dde7939096ba1908
MathisBurger <handymathis@gmail.com>
2021-01-11 21:27:13 +0100

    preparing move to backend

--- src/classes/var.ts
+++ src/classes/var.ts
@@ -1,5 +1,5 @@
 export class Var {
-  public API_Origin = 'http://localhost:8080';
+  public API_Origin = 'http://localhost:8080/api';
 
   constructor() {
     //this.API_Origin = location.protocol + '//' + location.hostname + '/api';
