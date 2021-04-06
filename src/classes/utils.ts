bdb5c8f495e9ce7d738f871b4e9fa3ce0141896f
Mathis <handymathis@gmail.com>
2021-04-06 18:45:43 +0200

    updated utils.ts for password validation

--- src/classes/utils.ts
+++ src/classes/utils.ts
@@ -13,4 +13,47 @@ export class Utils {
     }
     return arr;
   }
+
+  // password validation
+  // checks if the password has more than 8 chars
+  // has at least 1 special char
+  // has at least one number
+  validatePassword(pwd: string): any[] {
+    let spl = pwd.split('');
+
+    if (spl.length < 7) {
+      return [false, 'Your password must have at least 8 characters'];
+    }
+
+    const SPECIAL_CHARS: string[] = ['!', '§', '$', ')', '(', '?', '=', '^', '#', '+', '*', '-', '/', ';'];
+
+    let specialCounter = 0;
+
+    for (let element in spl) {
+      for (let char in SPECIAL_CHARS) {
+        if (element == char) { specialCounter++; }
+      }
+    }
+
+    if (specialCounter < 1) {
+      return [false, 'Your password must contain at least one special character'];
+    }
+
+    const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let numberCounter = 0;
+
+    for (let element in spl) {
+      for (let number in NUMBERS) {
+        if (element == number) { numberCounter++; }
+      }
+    }
+
+    if (numberCounter < 1) {
+      return [false, 'Your password must contain at least one number'];
+    }
+
+    return [true, ''];
+  }
+
 }
